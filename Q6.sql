968dd194d723b24213b34f8b66928c1964a532a6
Dhanyagb <109624454+Dhanyagb@users.noreply.github.com>
2022-08-03 10:35:33 -0400

    Update Q6.sql

--- Q6.sql
+++ Q6.sql
@@ -5,13 +5,13 @@ GO
 
 SELECT wsi.[StockItemID], wsi.[StockItemName], so.[OrderDate], c.[CityName], sp.[StateProvinceName]
 FROM [Warehouse].[StockItems] wsi
-JOIN [Sales].[OrderLines] sol
+INNER JOIN [Sales].[OrderLines] sol
 ON wsi.StockItemID = sol. StockItemID
-JOIN[Sales].[Orders] so
+INNER JOIN[Sales].[Orders] so
 ON sol.OrderID = so.OrderID
-JOIN [Application].[Cities] c
+INNER JOIN [Application].[Cities] c
 ON so.LastEditedBy = c.LastEditedBy
-JOIN [Application].[StateProvinces] sp
+INNER JOIN [Application].[StateProvinces] sp
 ON c.StateProvinceID = sp.StateProvinceID
 WHERE sp.StateProvinceName != ('ALABAMA''GEORGIA') AND YEAR (so.OrderDate) != '2014'
 ORDER BY StateProvinceName;
