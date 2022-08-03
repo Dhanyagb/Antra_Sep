28d89209ae71d3137dab5b18ca757c7ecfcaf425
Dhanyagb <109624454+Dhanyagb@users.noreply.github.com>
2022-08-03 10:36:20 -0400

    Update Q8.sql

--- Q8.sql
+++ Q8.sql
@@ -4,8 +4,8 @@ GO
 
 SELECT DATEDIFF(MM,si.OrderDate,si.ExpectedDeliveryDate) AS Avg_dates_months ,sp.[StateProvinceName],si.[OrderDate], si.[ExpectedDeliveryDate]
 FROM [Application].[StateProvinces] sp
-JOIN [Sales].[CustomerTransactions] ct
+INNER JOIN [Sales].[CustomerTransactions] ct
 ON sp.LastEditedBy = ct.LastEditedBy
-JOIN [Sales].[Orders] si
+INNER JOIN [Sales].[Orders] si
 ON ct.CustomerID = si.CustomerID
 ORDER BY sp.StateProvinceName;
